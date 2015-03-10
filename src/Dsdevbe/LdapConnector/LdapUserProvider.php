7d3f2ec4b6cab3714360d447f273ae5e7a7053f9
Wun Chiou <wun@utdallas.edu>
2015-03-10 17:05:44 -0500

    Works with Laravel stock User scaffold

--- src/Dsdevbe/LdapConnector/LdapUserProvider.php
+++ src/Dsdevbe/LdapConnector/LdapUserProvider.php
@@ -1,5 +1,6 @@
 <?php namespace Dsdevbe\LdapConnector;
 
+use App\User;
 use adLDAP\adLDAP;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\UserProvider as UserProviderInterface;
@@ -32,16 +33,19 @@ class LdapUserProvider implements UserProviderInterface {
      */
     public function retrieveById($identifier)
     {
+        /*        
         $userInfo = $this->adldap->user()->info($identifier, array('*'))[0];
-        
+
         $credentials = array();
-        $credentials['name'] = $identifier;
+        $credentials['netid'] = $identifier;
         
         foreach($userInfo as $key => $value){
             $credentials[$key] = $value[0];
         }
 
         return new LdapUser($credentials);
+        */
+        return User::find($identifier);
     }
 
     /**
@@ -77,13 +81,37 @@ class LdapUserProvider implements UserProviderInterface {
         $password = $credentials['password'];
         $dn = $this->adldap->user()->dn($name);
         if ($this->adldap->authenticate($dn, $password)) {
-            $userInfo = $this->adldap->user()->info($credentials['name'], array('*'))[0];
+            $userInfo = $this->adldap->user()->info($credentials['name'], array('mail','cn'))[0];
 
             foreach($userInfo as $key => $value){
                 $credentials[$key] = $value[0];
             }
 
-            return new LdapUser($credentials);
+            $user = User::where('name','=',$name);
+            if ($user->exists()) {
+                return $user->first();
+            }
+            else {
+                if (array_key_exists('mail',$credentials)) {
+                    $credentials['email'] = $credentials['mail'];
+                } else {
+                    $credentials['email'] = 'email_not_found';
+                }
+                if (array_key_exists('cn',$credentials)) {
+                    $credentials['display_name'] = $credentials['cn'];
+                } else {
+                    $credentials['display_name'] = 'name_not_found';
+                }
+                if (array_key_exists('password',$credentials)) {
+                    $credentials['password'] = bcrypt($credentials['password']);
+                } else {
+                    $credentials['password'] = 'password_not_found';
+                }
+                $newuser = new User($credentials);
+                $newuser->save();
+                return $newuser;
+            }
+            //return new LdapUser($credentials);
         }
     }
 
